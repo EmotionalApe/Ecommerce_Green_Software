b2a1373236d6ce5356a25f5b40ad6d4b1de09399
Maruf Khan <maruf1611khan@gmail.com>
2024-04-09 13:38:28 +0530

    added getCart and addToCart functionality

--- flaskBackend.py
+++ flaskBackend.py
@@ -53,9 +53,54 @@ def login():
     if not checkpw(password.encode('utf-8'), user['password']):
         return jsonify({"message": "Username or password is incorrect"}), 401
     
-    token = encode({'id': str(user['_id'])}, app.config['SECRET_KEY'], algorithm='HS256')
+    token = encode({'username': str(user['username'])}, app.config['SECRET_KEY'], algorithm='HS256')
     return jsonify({"token": token, "userID": str(user['_id'])}), 200
     # return jsonify({"message": "User logged in successfully"}), 201
+
+@app.route("/add_to_cart", methods=['POST'])
+def add_to_cart():
+    data = request.get_json()
+    token = data['token']
+    item = data['item']
+    
+    try:
+        decoded_token = decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        username = decoded_token['username']
+    except Exception as e:
+        return jsonify({"message": "Invalid token"}), 401
+    
+    # Check if the user's cart exists, create it if not
+    user_cart = db.cart.find_one({"username": username})
+    if not user_cart:
+        user_cart = {"username": username, "items": []}
+    
+    # Add the item to the user's cart
+    user_cart['items'].append(item)
+    
+    # Upsert the user's cart
+    db.cart.replace_one({"username": username}, user_cart, upsert=True)
+    
+    return jsonify({"message": "Item added to cart successfully"}), 200
+
+@app.route("/get_cart", methods=['POST'])
+def get_cart():
+    data = request.get_json()
+    token = data.get('token')
+
+    try:
+        decoded_token = decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        username = decoded_token['username']
+    except Exception as e:
+        return jsonify({"message": "Invalid token"}), 401
+    
+    # Check if the user's cart exists
+    user_cart = db.cart.find_one({"username": username},{"_id": 0})
+    if not user_cart:
+        return jsonify({"message": "No items in cart"}), 200
+    else:
+        # Return the user's cart
+        return jsonify(user_cart), 200
+
 
 if __name__ == "__main__":
     app.run(debug=True)
